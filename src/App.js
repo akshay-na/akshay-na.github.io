a7eb321727bf7ff212477345dd5bab43ab1f526b
akshay-na <akshayna04698@gmail.com>
2021-12-28 21:15:14 +0530

    Added Preloader svg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import Preloader from "../src/components/preloader/Pre";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
+      <Preloader load={load} />
       <Main />
     </div>
   );
---
+++ src/components/preloader/Pre.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Pre.css";
+
+
+function Pre(props) {
+  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+}
+
+export default Pre;
