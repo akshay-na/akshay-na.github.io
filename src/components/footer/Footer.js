4709b5d0866d1cd60c524a3b0e9fe8cb39f1212f
akshay-na <67007337+akshay-na@users.noreply.github.com>
2022-01-06 17:35:04 +0530

    Added hyperlink in the footer name

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,10 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by Akshay N")}
+          {emoji("Made with ❤️ by ")}
+          <a href="https://github.com/akshay-na/akshay-na.github.io">
+            Akshay N
+          </a>
         </p>
         <p className={isDark ? "dark-mode footer-text-p" : "footer-text-p"}>
           Theme by{" "}
